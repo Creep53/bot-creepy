50d71b2a81da55873efaa9e662b839c5df292bb3
Creep53 <creepminecraft53@gmail.com>
2018-11-03 18:26:46 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,39 +9,11 @@ bot.on('ready', function () {
 });
 
 bot.on('message', msg => {
-    
     if(message.content === "Bonjour"){
         message.reply("Salut");
     }
 	if(message.content === "Comment ça va ?"){
         message.reply("trql trql toi même tu sais et toi petit con ?");
-    }
-	if(message.content === "Creep_1"){
-        message.reply("Il est beau lui fais gaffe");
-    }
-	if(message.content === "Suceur"){
-        message.reply(":robot: moi jpeux pas mais toi oui :heart: ");
-    }	
-
-	if(message.content === "test"){
-        message.reply("ptdr ta cru quoi toi");
-    }	
-	
-	if(message.content === "tez"){
-        message.reply("il est no.. blanc ta cru quoi c'est un vieux un cachet d'aspirine");
-    }		
-	
-	if(message.content === "bite"){
-        message.reply("Ici c'est moi qui a la plus grosse ok ");
-    }
-
-	if(message.content === "xD"){
-        message.reply("pk tu rigoles sale batard");
-    }
-
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
     }
 
 });
