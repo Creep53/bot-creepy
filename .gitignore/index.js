47cc8d01c2ff767490e737bc89b8f5264986bea0
Creep53 <creepminecraft53@gmail.com>
2018-11-06 17:33:43 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,11 @@
-const Discord = require('discord.js');
+const Discord = require('./discord.js/');
 const bot = new Discord.Client();
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+const prefix = ("Cp ");
 
 bot.on('ready', function () {
-    console.log("Je suis prêt à être utilisé.")
+   console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame("Cp help");
     bot.user.setActivity('rien').catch(console.error)
 });
 
@@ -15,10 +16,22 @@ bot.on('message', msg => {
     if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
     }
-    if (msg.content === prefix + "help"){
-        msg.channel.send("")
-        console.log("Une personne a demandé pour aller sur ton site.")
+   	
+    if(message.content === prefix + "help") {
+      var aide_embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`:robot: Voici mes catégories d'aide !`)
+      .setDescription(`Voici mes commandes disponible :`)
+      .setThumbnail(message.author.avatarURL)
+      .addField(":tada: Fun", "Fais `Cp fun` pour voir mes commandes d'animation !")
+      .setFooter("Menu d'aide - By Creep_1")
+      .setTimestamp()
+      console.log("Une personne a utilise")
+      message.channel.send(aide_embed);
+
     }
+       
+    
 
 });
 
